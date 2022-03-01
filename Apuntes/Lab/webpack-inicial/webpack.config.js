f8b37a443f9287e0e8f93efcb3b4ba7ca4d37136
Dieg0Code <diegoobando20@gmail.com>
2022-03-01 19:54:05 -0300

    webpack - archivo de estilos global

--- Apuntes/Lab/webpack-inicial/webpack.config.js
+++ Apuntes/Lab/webpack-inicial/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
@@ -18,7 +19,12 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                exclude: /styles.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
             },
         ]
     },
@@ -32,6 +38,11 @@ module.exports = {
             title: 'Mi Webpack App',
             // filename: 'index.html',
             template: './src/index.html',
+        }),
+
+        new MiniCssExtract({
+            filename: 'styles.css',
+            ignoreOrder: false,
         })
     ],
 }
