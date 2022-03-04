bfc1b0d66f723fa4b0f542fa83d85d1cf0a99da0
Dieg0Code <diegoobando20@gmail.com>
2022-03-04 07:00:08 -0300

    CRUD - crear usuario

--- Apuntes/Lab/http/src/index.js
+++ Apuntes/Lab/http/src/index.js
@@ -2,3 +2,8 @@ import * as CRUD from './js/crud-provider';
 
 
 CRUD.getUsuario( 1 ).then( console.log );
+
+CRUD.crearUsuario({
+    name: 'Diego',
+    job: 'Desarrollador'
+}).then( console.log );
--- Apuntes/Lab/http/src/js/crud-provider.js
+++ Apuntes/Lab/http/src/js/crud-provider.js
@@ -5,11 +5,24 @@ const getUsuario = async ( id ) => {
 
     const res = await fetch( `${ urlCRUD }/${ id }` );
     const { data } = await res.json();
-
     return data;
+}
 
+const crearUsuario = async ( usuario ) => {
+
+    const res = await fetch( urlCRUD, {
+
+        method: 'POST',
+        body: JSON.stringify( usuario ),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return await res.json();
 }
 
 export {
-    getUsuario
+    getUsuario,
+    crearUsuario
 }
